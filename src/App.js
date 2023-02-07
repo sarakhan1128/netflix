9a397127ea8b47bb9db8879412737d9af373ceda
kinggsara <itssara238@gmail.com>
2023-02-07 20:23:11 +0530

    added code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Row from './Row';
+import requests from './requests';
+import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true} />
+      <Row title="Trending Now"fetchUrl={requests.fetchTrending} isLargeRow={true}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} isLargeRow={true}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} isLargeRow={true}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} isLargeRow={true}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} isLargeRow={true}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} isLargeRow={true}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} isLargeRow={true}/>
     </div>
   );
 }
 
 export default App;
+
